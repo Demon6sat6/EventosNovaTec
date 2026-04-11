215797e8bf5807816d5eb5e566de1f1c59714761
Demon6sat6 <idgfshv@gmail.com>
2026-04-11 14:04:36 -0500

    chore: remove .vercel from git, update .gitignore

--- .vercel/output/_functions/chunks/stripe_CmXxU-t9.mjs
+++
@@ -1,5 +0,0 @@
-import Stripe from 'stripe';
-
-const stripe = new Stripe("sk_test_...");
-
-export { stripe as s };
--- .vercel/output/_functions/entry.mjs
+++
@@ -1,1 +0,0 @@
-export { e as default } from './chunks/entrypoint_B7Ywt9HA.mjs';
